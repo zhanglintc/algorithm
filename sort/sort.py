39dfebd7c202bacd738926980e91d436c5dfd882
zhanglintc <zhanglintc623@gmail.com>
2021-03-19 22:29:00 +0800

    travis

--- sort/sort.py
+++ sort/sort.py
@@ -3,6 +3,17 @@
 # -*- mode: python -*-
 # vi: set ft=python :
 
+def timer(func):
+    import functools
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        import time
+        t1 = time.time()
+        func(*args, **kwargs)
+        t2 = time.time()
+        print(f"{func.__name__}: {t2 - t1} secs")
+    return wrapper
+
 def swap_sort(a):
     n = len(a)
     for i in range(n):
@@ -10,6 +21,7 @@ def swap_sort(a):
             if a[i] > a[j]:
                 a[i], a[j] = a[j], a[i]
 
+@timer
 def bubble_sort(a):
     n = len(a)
     for i in range(n):
@@ -17,9 +29,41 @@ def bubble_sort(a):
             if a[j] > a[j+1]:
                 a[j], a[j+1] = a[j+1], a[j]
 
+@timer
+def selection_sort(a):
+    n = len(a)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            min_idx = min_idx if a[min_idx] < a[j] else j
+        a[i], a[min_idx] = a[min_idx], a[i]
+
+@timer
+def insertion_sort(a):
+    n = len(a)
+    for i in range(n):
+        pre_idx = i - 1
+        current = a[i]
+        while pre_idx >= 0 and a[pre_idx] > current:
+            a[pre_idx+1] = a[pre_idx]
+            pre_idx -= 1
+        a[pre_idx+1] = current
+
+@timer
+def heap_sort(a):
+    import heapq
+    h = []
+    for n in a:
+        heapq.heappush(h, n)
+    a.clear()
+    for _ in range(len(h)):
+        n = heapq.heappop(h)
+        a.append(n)
+
+@timer
 def qsort(a):
     import sys
-    sys.setrecursionlimit(3000)
+    sys.setrecursionlimit(5000)
     def _qsort(a, bl, br):
         if bl >= br:
             return
--- ut.py
+++ ut.py
@@ -15,35 +15,58 @@ class AlgorithmSortTest(BaseMmrzTestCase):
         import sort.sort as _sort
         cls._sort = _sort
         import random
-        cls._array = [random.randint(0, 100000) for _ in range(1000)]
+        cls._array = [random.randint(0, 100000) for _ in range(2000)]
         instance = super(cls.__class__, cls).__new__(cls)
         return instance
 
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
 
-    # @unittest.skip('bug')
-    def test_swap_sort(self):
-        by_swap = self._array[:]
-        by_system = self._array[:]
-        self._sort.swap_sort(by_swap)
-        by_system.sort()
-        self.assertEqual(by_swap, by_system)
+    @unittest.skip('pass')
+    def test_skip(self):
+        pass
 
-    # @unittest.skip('bug')
-    def test_bubble_sort(self):
-        by_bubble = self._array[:]
+    def test_swap_sort(self):
+        by_testee = self._array[:]
         by_system = self._array[:]
-        self._sort.bubble_sort(by_bubble)
+        self._sort.swap_sort(by_testee)
         by_system.sort()
-        self.assertEqual(by_bubble, by_system)
+        self.assertEqual(by_testee, by_system)
+
+    def test_bubble_sort(self):
+        by_testee = self._array[:]
+        by_system = self._array[:]
+        self._sort.bubble_sort(by_testee)
+        by_system.sort()
+        self.assertEqual(by_testee, by_system)
+
+    def test_selection_sort(self):
+        by_testee = self._array[:]
+        by_system = self._array[:]
+        self._sort.selection_sort(by_testee)
+        by_system.sort()
+        self.assertEqual(by_testee, by_system)
+
+    def test_insertion_sort(self):
+        by_testee = self._array[:]
+        by_system = self._array[:]
+        self._sort.insertion_sort(by_testee)
+        by_system.sort()
+        self.assertEqual(by_testee, by_system)
+
+    def test_heap_sort(self):
+        by_testee = self._array[:]
+        by_system = self._array[:]
+        self._sort.heap_sort(by_testee)
+        by_system.sort()
+        self.assertEqual(by_testee, by_system)
 
     def test_qsort(self):
-        by_qsort = self._array[:]
+        by_testee = self._array[:]
         by_system = self._array[:]
-        self._sort.qsort(by_qsort)
+        self._sort.qsort(by_testee)
         by_system.sort()
-        self.assertEqual(by_qsort, by_system)
+        self.assertEqual(by_testee, by_system)
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
